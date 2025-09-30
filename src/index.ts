45dc693ac564afc125c1fb9bdde95c11b8150849
Arjen Postma <info@postma-dev.nl>
2025-09-30 20:11:52 +0200

    Fixes font issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import './styles/reset.css';
 import '../node_modules/modern-normalize/modern-normalize.css';
-import './styles/inter.css';
 import './styles/variables.css';
 
 export { default as StButton } from './components/button/StButton.vue';
