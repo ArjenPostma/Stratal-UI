123c698fbfc1b53c0b7a6f1189ba2a0b9e72acb6
Arjen Postma <82020287+ArjenPostma@users.noreply.github.com>
2025-12-06 14:54:56 +0100

    Updated packages (#14)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import path from 'path';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
     root: './playground',
     plugins: [vue()],
     resolve: {
         alias: {
-            'stratal-ui': path.resolve(__dirname, './src'),
+            'stratal-ui': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
 });
