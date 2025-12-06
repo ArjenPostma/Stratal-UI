c3df1d873221595f1d7156dc4834382d82f542c2
Arjen Postma <info@postma-dev.nl>
2025-12-06 17:40:21 +0100

    Moved full enums to core source

---
+++ src/enums/variant.ts
@@ -0,0 +1,12 @@
+export enum StVariant {
+    GREY = 'GREY',
+    RED = 'RED',
+    PINK = 'PINK',
+    PURPLE = 'PURPLE',
+    CYAN = 'CYAN',
+    BLUE = 'BLUE',
+    TEAL = 'TEAL',
+    GREEN = 'GREEN',
+    YELLOW = 'YELLOW',
+    ORANGE = 'ORANGE',
+}
