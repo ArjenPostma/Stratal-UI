81c795701bc30f553164496b2d09ddb2ef038798
Arjen Postma <info@postma-dev.nl>
2025-12-06 17:41:13 +0100

    Added checkbox playground view

--- playground/src/router.ts
+++ playground/src/router.ts
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HomeView from './views/HomeView.vue';
 import ButtonView from './views/ButtonView.vue';
 import InputView from './views/InputView.vue';
+import CheckboxView from "./views/CheckboxView.vue";
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/button', component: ButtonView },
     { path: '/input', component: InputView },
+    { path: '/checkbox', component: CheckboxView },
 ];
 
 export const router = createRouter({
---
+++ playground/src/views/CheckboxView.vue
@@ -0,0 +1,130 @@
+<template>
+    <div
+        v-for="variant in Object.values(StVariant)"
+        :key="variant"
+        style="display: flex; flex-direction: column; gap: 8px; margin-bottom: 8px;"
+    >
+        <div style="display: flex; flex-direction: row; gap: 8px">
+            <div
+                v-for="style in Object.values(StCheckboxStyle)"
+                :key="style"
+                style="display: flex; flex-direction: row;"
+            >
+                <StCheckbox
+                    v-for="size in Object.values(StCheckboxSize)"
+                    :key="size"
+                    :size="size"
+                    :variant="variant"
+                    :type="style"
+                    style="margin-left: 4px; margin-right: 4px"
+                >
+                    Button
+                </StCheckbox>
+            </div>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 8px">
+            <div
+                v-for="style in Object.values(StCheckboxStyle)"
+                :key="style"
+                style="display: flex; flex-direction: row;"
+            >
+                <StCheckbox
+                    v-for="size in Object.values(StCheckboxSize)"
+                    :key="size"
+                    :size="size"
+                    :variant="variant"
+                    :type="style"
+                    style="margin-left: 4px; margin-right: 4px"
+                    disabled
+                >
+                    Button
+                </StCheckbox>
+            </div>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 8px">
+            <div
+                v-for="style in Object.values(StCheckboxStyle)"
+                :key="style"
+                style="display: flex; flex-direction: row;"
+            >
+                <StCheckbox
+                    v-for="size in Object.values(StCheckboxSize)"
+                    :key="size"
+                    :size="size"
+                    :variant="variant"
+                    :type="style"
+                    style="margin-left: 4px; margin-right: 4px"
+                    v-model="checked"
+                >
+                    Button
+                </StCheckbox>
+            </div>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 8px">
+            <div
+                v-for="style in Object.values(StCheckboxStyle)"
+                :key="style"
+                style="display: flex; flex-direction: row;"
+            >
+                <StCheckbox
+                    v-for="size in Object.values(StCheckboxSize)"
+                    :key="size"
+                    :size="size"
+                    :variant="variant"
+                    :type="style"
+                    style="margin-left: 4px; margin-right: 4px"
+                    v-model="checked"
+                    disabled
+                >
+                    Button
+                </StCheckbox>
+            </div>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 8px">
+            <div
+                v-for="style in Object.values(StCheckboxStyle)"
+                :key="style"
+                style="display: flex; flex-direction: row;"
+            >
+                <StCheckbox
+                    v-for="size in Object.values(StCheckboxSize)"
+                    :key="size"
+                    :size="size"
+                    :variant="variant"
+                    :type="style"
+                    style="margin-left: 4px; margin-right: 4px"
+                    indeterminate
+                >
+                    Button
+                </StCheckbox>
+            </div>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 8px">
+            <div
+                v-for="style in Object.values(StCheckboxStyle)"
+                :key="style"
+                style="display: flex; flex-direction: row;"
+            >
+                <StCheckbox
+                    v-for="size in Object.values(StCheckboxSize)"
+                    :key="size"
+                    :size="size"
+                    :variant="variant"
+                    :type="style"
+                    style="margin-left: 4px; margin-right: 4px"
+                    indeterminate
+                    disabled
+                >
+                    Button
+                </StCheckbox>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { StCheckbox, StCheckboxSize, StVariant, StCheckboxStyle } from 'stratal-ui';
+import {ref} from "vue";
+
+const checked = ref(true);
+</script>
